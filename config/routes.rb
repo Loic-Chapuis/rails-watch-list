3910f880d282b231a2e54bf7013fc20f19393500
loic chapuis <loickoxxfox@hotmail.com>
2021-11-18 12:15:42 +0100

    models and migration done

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'bookmarks/comments'
-  get 'lists/name'
-  get 'movies/title'
-  get 'movies/overview'
-  get 'movies/poster_url'
-  get 'movies/rating'
+  get 'bookmarks/comments', to: 'bookmarks#comments'
+  get 'lists/name', to: 'lists#name'
+  get 'movies/title', to: 'movies#title'
+  get 'movies/overview', to: 'movies#overview'
+  get 'movies/poster_url', to: 'movies#poster_url'
+  get 'movies/rating', to: 'movies#rating'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'movies#home'
+
+
 end

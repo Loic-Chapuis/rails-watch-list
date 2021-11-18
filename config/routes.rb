46267ad723d1f6dcfd30e909183098f0055f479a
loic chapuis <loickoxxfox@hotmail.com>
2021-11-18 14:27:24 +0100

    middle

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  get 'bookmarks/comments', to: 'bookmarks#comments'
-  get 'lists/name', to: 'lists#name'
-  get 'movies/title', to: 'movies#title'
-  get 'movies/overview', to: 'movies#overview'
-  get 'movies/poster_url', to: 'movies#poster_url'
-  get 'movies/rating', to: 'movies#rating'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'movies#home'
-
-
+  root to: 'lists#index'
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
+  end
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end

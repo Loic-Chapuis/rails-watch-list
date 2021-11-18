023ea1bef6f3b75492a6a74f9579cd8b739ce2cd
loic chapuis <loickoxxfox@hotmail.com>
2021-11-18 10:56:16 +0100

    generation controller and model

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,13 @@
+class MoviesController < ApplicationController
+  def title
+  end
+
+  def overview
+  end
+
+  def poster_url
+  end
+
+  def rating
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'bookmarks/comments'
+  get 'lists/name'
+  get 'movies/title'
+  get 'movies/overview'
+  get 'movies/poster_url'
+  get 'movies/rating'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
